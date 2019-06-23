c447683d2c7f12439a72ff2de8ab1eece2e0cd29
Michael Hughes <mhughes36@live.co.uk>
2019-06-23 20:00:52 +0100

    create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
-  def index
+  def new
+    @post = Post.new
   end
+  
+  def create
+    @post = Post.create!(post_params)
+    # @post.images.attach(params[:images])
+    redirect_to posts_url
+  end
+
+  def index
+    @posts = Post.all
+  end
+ 
+  private
+    def post_params
+      params.require(:post).permit(:title, :content, images: [])
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-
+  has_many_attached :images
 
 end
